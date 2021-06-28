d1e21c2df0c854ca89fd291c2336e2dfac0cd98d
이상민 <eksajrm0624@naver.com>
2021-06-28 18:17:25 +0900

    Basic Comparative Operations
    CodeUp - C1049-52

---
+++ C1049-52.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    /*
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a>b);
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a==b);
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", b >= a);
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a!=b);
+
+    */
+
+    return 0;
+    
+}
