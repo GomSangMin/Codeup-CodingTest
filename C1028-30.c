72df1b61474a50bde55ba971edd0c1e8198e5028
이상민 <eksajrm0624@naver.com>
2021-06-28 15:34:52 +0900

    Basic Data Type
    CodeUp - C1028-30

---
+++ C1028-30.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    /*
+
+    unsigned int a;
+    scanf("%u", &a);
+    printf("%u", a);
+
+    double d;
+    scanf("%lf", &d);
+    printf("%.11lf", d);
+
+    long long int n;
+    scanf("%lld", &n);
+    printf("%lld", n);
+
+    */
+
+    return 0;
+}
