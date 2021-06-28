fb7522ff8905577ce96ec1918d76d1a71f2c920d
이상민 <eksajrm0624@naver.com>
2021-06-28 15:06:54 +0900

    Basic output 06-08
    CodeUp - C1006 - 08

---
+++ C1006-8.c
@@ -0,0 +1,19 @@
+/* C1006-8 출력하기 06-08 */
+
+#include <stdio.h>
+
+int main()
+{
+  /* C1006 */
+  printf("\"!@#$%%^&*()\"\n");
+
+  /* C1007 */
+  printf("C:\\Download\\hello.cpp\n");
+
+  /* C1008 */
+  printf("\u250C\u252C\u2510\n");
+  printf("\u251C\u253C\u2524\n");
+  printf("\u2514\u2534\u2518");
+
+  return 0;
+}
