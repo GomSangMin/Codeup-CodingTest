389bc3845d646d9d47a6c62d554e4f997bc12fce
이상민 <eksajrm0624@naver.com>
2021-06-28 18:11:55 +0900

    Basic Bitshift Operation
    CodeUp - C1047-48

---
+++ C10047-48.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    /*
+
+    int a;
+    scanf("%d", &a);
+    printf("%d", a<<1);
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a<<b);
+
+    */
+
+    return 0;
+
+}
