e0ff4919ce61c5c9421219fde864b323fb9638fb
이상민 <eksajrm0624@naver.com>
2021-06-28 17:09:54 +0900

    Basic convert foundation output
    CodeUp - C1031 - 37

---
+++ .vscode/C1031-37.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+
+    /*
+
+    int a;
+    scanf("%d", &a);
+    printf("%o", a);
+
+    int n;
+    scanf("%d", &n);
+    printf("%x", n);
+
+    int n;
+    scanf("%d", &n);
+    printf("%X", n);
+
+    int n;
+    scanf("%o", &n);
+    printf("%d", n);
+
+    int n;
+    scanf("%x", &n);
+    printf("%o", n);
+
+    char c;
+    scanf("%c", &c);
+    printf("%d", c);
+
+    int n;
+    scanf("%d", &n);
+    printf("%c", n);
+
+    */
+   
+    return 0;   
+}
