ff8fac93df7bd3565d74abeff880270bf3d3e5f8
이상민 <eksajrm0624@naver.com>
2021-06-30 08:13:23 +0900

    Basic-Input-Output
    .

---
+++ Basic-DataType.c
@@ -0,0 +1,30 @@
+/*
+
+Basic-Data Type
+
+CodeUp - C1028 ~ C1030
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    /*
+
+    unsigned int a;
+    scanf("%u", &a);
+    printf("%u", a);
+
+    double d;
+    scanf("%lf", &d);
+    printf("%.11lf", d);
+
+    long long int n;
+    scanf("%lld", &n);
+    printf("%lld", n);
+
+    */
+
+    return 0;
+}
