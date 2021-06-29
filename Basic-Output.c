c37a7e7c81dc60ff82cf35a5379d84178eff5e80
이상민 <eksajrm0624@naver.com>
2021-06-30 08:16:05 +0900

    Basic-Output
    .

---
+++ Basic-Output.c
@@ -0,0 +1,41 @@
+/* 
+
+Basic-Output
+
+CodeUp - C1001 ~ C1008
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  /* C1001 */
+  printf("Hello");
+
+  /* C1002 */
+  printf("Hello World");
+
+  /* C1003 */
+  printf("Hello\n");
+  printf("World");
+
+  /* C1004 */
+  printf("\'Hello\'\n");
+
+  /* C1005 */
+  printf("\"Hello World\"\n");
+
+  /* C1006 */
+  printf("\"!@#$%%^&*()\"\n");
+
+  /* C1007 */
+  printf("C:\\Download\\hello.cpp\n");
+
+  /* C1008 */
+  printf("\u250C\u252C\u2510\n");
+  printf("\u251C\u253C\u2524\n");
+  printf("\u2514\u2534\u2518");
+
+  return 0;
+}
