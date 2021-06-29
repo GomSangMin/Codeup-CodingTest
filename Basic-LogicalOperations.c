7721787ee728092ba772e368ea29bd3581a20523
이상민 <eksajrm0624@naver.com>
2021-06-30 07:57:54 +0900

    Basic-Logical Operations
    CodeUp - C1053 ~ 58

---
+++ Basic-LogicalOperations.c
@@ -0,0 +1,43 @@
+/*
+
+Basic-Logical Operations
+
+CodeUp - C1053 ~ C1058
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    /*
+
+    int a;
+    scanf("%d", &a);
+    printf("%d", !a);
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a&&b);
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a||b);
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", (a&&!b)||(!a&&b));
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", (a&&b)||(!a&&!b));    
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", !(a||b)); 
+
+    */
+
+    return 0;
+
+}
