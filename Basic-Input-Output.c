254ddd036dd81f0e2e4521d47d3965639ca3c90b
이상민 <eksajrm0624@naver.com>
2021-06-30 08:13:45 +0900

    Basic-Input-Output
    .

---
+++ Basic-Input-Output.c
@@ -0,0 +1,86 @@
+/*
+
+Basic-Input/Output
+
+CodeUp - C1008 ~ C1027
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    printf("%d", n);
+
+    /*
+
+    char n;
+    scanf("%c",&n);
+    printf("%c", n);
+
+    float n;
+    scanf("%f",&n);
+    printf("%f", n);
+
+    int a, b;
+    scanf("%d%d",&a, &b);
+    printf("%d %d", a, b);
+
+    char a, b;
+    scanf("%c %c",&a, &b);
+    printf("%c %c", b, a);
+
+    float a;
+    scanf("%f",&a);
+    printf("%.2f",a);
+
+    int a;
+    scanf("%d",&a);
+    printf("%d %d %d",a, a, a);
+
+    int a, b;
+    scanf("%d:%d",&a, &b);
+    printf("%d:%d",a, b);
+
+    int y, m, d;
+    scanf("%d.%d.%d",&y, &m, &d);
+    printf("%04d.%02d.%02d",y, m, d);
+
+    int f,l;
+    scanf("%d-%d",&f, &l);
+    printf("%06d%d", f,l);
+
+    char data[51]="";
+    scanf("%s",data);
+    printf("%s", data);
+
+    char data[2001]="";
+    fgets(data,2000,stdin);
+    printf("%s", data);
+
+    int a,b;
+    scanf("%d.%d", &a, &b);
+    printf("%d\n%d", a, b);
+
+    char data[30] = "";
+    scanf("%s", data);
+    for(int i=0; data[i]!='\0'; i++) {
+        printf("\'%c\'\n", data[i]);
+    }
+
+    int a,b,c,d,e = 0;
+    scanf("%1d%1d%1d%1d%1d", &a, &b, &c, &d, &e);
+    printf("[%d]\n[%d]\n[%d]\n[%d]\n[%d]", a*10000, b*1000, c*100, d*10, e);
+
+    int h, m, s;
+    scanf("%d:%d:%d", &h, &m, &s);
+    printf("%d",m);
+
+    int y, m, d;
+    scanf("%d.%d.%d", &y, &m, &d);
+    printf("%02d-%02d-%d",d,m,y);
+
+    */
+
+    return 0;
+}
