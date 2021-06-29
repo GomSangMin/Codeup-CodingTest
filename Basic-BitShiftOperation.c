eb1333fda94d697830b12f0fc327ce7cf6895e6e
이상민 <eksajrm0624@naver.com>
2021-06-30 08:12:49 +0900

    Basic-LogicalOperations
    .

---
+++ Basic-BitShiftOperation.c
@@ -0,0 +1,26 @@
+/*
+
+Basic-Bit Shift Operation
+
+CodeUp - C1047 ~ C1048
+
+*/
+#include <stdio.h>
+
+int main() {
+
+    /*
+
+    int a;
+    scanf("%d", &a);
+    printf("%d", a<<1);
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a<<b);
+
+    */
+
+    return 0;
+
+}
