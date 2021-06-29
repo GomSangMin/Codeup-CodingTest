d5bc80b74743d65d4542bf64767bc0bea695da57
이상민 <eksajrm0624@naver.com>
2021-06-30 08:16:25 +0900

    Basic-OutputConversion
    .

---
+++ Basic-OutputConversion.c
@@ -0,0 +1,46 @@
+/*
+
+Basic-Output Conversion
+
+CodeUp - C1031 ~ C1037
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    /*
+
+    int a;
+    scanf("%d", &a);
+    printf("%o", a);
+
+    int n;
+    scanf("%d", &n);
+    printf("%x", n);
+
+    int n;
+    scanf("%d", &n);
+    printf("%X", n);
+
+    int n;
+    scanf("%o", &n);
+    printf("%d", n);
+
+    int n;
+    scanf("%x", &n);
+    printf("%o", n);
+
+    char c;
+    scanf("%c", &c);
+    printf("%d", c);
+
+    int n;
+    scanf("%d", &n);
+    printf("%c", n);
+
+    */
+   
+    return 0;   
+}
