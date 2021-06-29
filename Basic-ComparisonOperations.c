6716d7e6f9643920ccf04e7259e5eda39122f89c
이상민 <eksajrm0624@naver.com>
2021-06-30 08:33:10 +0900

    Basic-ComparisonOperation
    .

--- Basic-ComparisonOperations.c
+++ Basic-ComparisonOperations.c
@@ -23,11 +23,11 @@ int main() {
     scanf("%d %d", &a, &b);
     printf("%d", b >= a);
 
+    */
+
     int a, b;
     scanf("%d %d", &a, &b);
     printf("%d", a!=b);
-
-    */
 
     return 0;
     
