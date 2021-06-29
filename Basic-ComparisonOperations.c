e2c95cbbbf481b67f4bde9764376de425109297f
이상민 <eksajrm0624@naver.com>
2021-06-30 08:13:10 +0900

    Basic-ComparisonOperation
    .

---
+++ Basic-ComparisonOperations.c
@@ -0,0 +1,34 @@
+/*
+
+Basic-Comparison Operations
+
+CodeUp - C1049 ~ C1052
+
+*/
+#include <stdio.h>
+
+int main() {
+
+    /*
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a>b);
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a==b);
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", b >= a);
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a!=b);
+
+    */
+
+    return 0;
+    
+}
