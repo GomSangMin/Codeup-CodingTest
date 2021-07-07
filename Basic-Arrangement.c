953c8b99edeac9e078b328d9aca8bf76a070edb1
이상민 <eksajrm0624@naver.com>
2021-07-07 10:33:32 +0900

    Basic-Arrangement
    Complete.

--- Basic-Arrangement.c
+++ Basic-Arrangement.c
@@ -57,25 +57,81 @@ int main() {
         printf("\n");
     }
 
-    */
-
     int s, x, y;
-    int a[20][20];
+    int a[20][20]={};
     for(int i = 1; i<=19; i++){     // 바둑판 입력 받기
         for(int j = 1; j<=19; j++){
             scanf("%d", &a[i][j]);
         }
     }
-    scanf("%d", &s); // 좌표입력받기.
-    for(int j = 1; j<=s; j++){
+    scanf("%d", &s); // 좌표개수입력받기.
+    for(int j = 1; j<=s; j++){  // 좌표입력받기
         scanf("%d %d", &x, &y);
         for(int j = 1; j<=19; j++){
             if(a[x][j]==0) { a[x][j]=1; }
             else a[x][j] = 0;
         }
-        for(int j = 1;);
+        for(int j = 1; j<=19; j++){
+            if(a[j][y]==0) { a[j][y]=1; }
+            else a[j][y] = 0;
+        }
+    }
+    for(int i = 1; i<=19; i++){
+        for(int j = 1; j<=19; j++){
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
     }
 
+    int h, w, n, l, d, x, y, board[100][100] = {0, };
+    scanf("%d %d", &h, &w);
+    scanf("%d", &n);
+    for(int i = 1; i<=n; i++){
+        scanf("%d %d %d %d", &l, &d, &x, &y);
+        if(d==0) {
+            for(int i = y; i<(y+l); i++) { board[x][i]=1;}
+        }
+        if(d==1) {
+            for(int i = x; i<(x+l); i++) { board[i][y]=1;}
+        }
+    }
+    for(int i = 1; i<=h; i++){
+        for(int j = 1; j<=w; j++){
+            printf("%d ", board[i][j]);
+        }
+        printf("\n");
+    }
+
+    */
+
+    int board[10][10] = {0, };
+    for(int i = 0; i<10; i++){
+        for(int j = 0; j<10; j++){
+            scanf("%d", &board[i][j]);
+        }
+    }
+    int i = 1, j = 1;
+    while(1){
+        if(board[i][j]==1) { 
+            i++;
+            j--;
+        }
+        else if(board[i][j]==0) { 
+            board[i][j] = 9; 
+            j++;
+        }
+        else { 
+            board[i][j] = 9;
+            break; 
+        }
+    }
+    
+    for(int i = 0; i<10; i++){
+        for(int j = 0; j<10; j++){
+            printf("%d ", board[i][j]);
+        }
+        printf("\n");
+    }
     
     return 0;
 }
